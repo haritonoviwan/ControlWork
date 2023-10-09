84537c1d2e21e391a03c8d78a83f75b1d555e111
Иван Харитонов <haritonoviwan@yandex.ru>
2023-10-09 12:36:15 +0300

    Описал метод ввода массива строк пользователем

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string[] GetArrayFromUser()
+{
+    Console.Write("Введите количество строк в массиве: ");
+    int n = int.Parse(Console.ReadLine());
+
+    string[] inputArray = new string[n];
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write($"Введите строку {i + 1}: ");
+        inputArray[i] = Console.ReadLine();
+    }
+    return inputArray;
+}
